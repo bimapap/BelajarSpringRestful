01b7f9b2674946c0c6b6dbb2921f4754dcab82dc
Bima Prasetya Adi Pratama <bimapap@gmail.com>
2022-07-17 14:43:38 +0700

    add lombok

--- src/main/java/bimapap/belajarspringrestful/model/Buku.java
+++ src/main/java/bimapap/belajarspringrestful/model/Buku.java
@@ -2,6 +2,8 @@ package bimapap.belajarspringrestful.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +14,8 @@ import java.util.Date;
 @Entity @Table(name = "books")
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt","updatedAt"},allowGetters = true)
+@Getter
+@Setter
 public class Buku {
 
     @Id
@@ -42,68 +46,4 @@ public class Buku {
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
     private Date updatedAt;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitleBook() {
-        return titleBook;
-    }
-
-    public void setTitleBook(String titleBook) {
-        this.titleBook = titleBook;
-    }
-
-    public String getNamaDepanPengarang() {
-        return namaDepanPengarang;
-    }
-
-    public void setNamaDepanPengarang(String namaDepanPengarang) {
-        this.namaDepanPengarang = namaDepanPengarang;
-    }
-
-    public String getNamaBelakangPengarang() {
-        return namaBelakangPengarang;
-    }
-
-    public void setNamaBelakangPengarang(String namaBelakangPengarang) {
-        this.namaBelakangPengarang = namaBelakangPengarang;
-    }
-
-    public Integer getStatusPeminjaman() {
-        return statusPeminjaman;
-    }
-
-    public void setStatusPeminjaman(Integer statusPeminjaman) {
-        this.statusPeminjaman = statusPeminjaman;
-    }
-
-    public String getNamaPeminjam() {
-        return namaPeminjam;
-    }
-
-    public void setNamaPeminjam(String namaPeminjam) {
-        this.namaPeminjam = namaPeminjam;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
